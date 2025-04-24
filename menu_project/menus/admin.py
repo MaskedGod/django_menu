4899a3e12f676d5b18cbd09875334f566bc712ca
MaskedGod <codegobbledygook@gmail.com>
2025-04-24 20:51:08 +0400

    feat: create models and admin panel

--- menu_project/menus/admin.py
+++ menu_project/menus/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Menu, MenuItem
+
+
+class MenuItemInline(admin.StackedInline):
+    model = MenuItem
+    extra = 1
+    show_change_link = True
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    inlines = [MenuItemInline]
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "menu", "parent", "url", "named_url")
+    list_filter = ("menu",)
+    search_fields = ("title", "url", "named_url")
--- menu_project/menus/models.py
+++ menu_project/menus/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="items")
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=200, blank=True)
+    named_url = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        unique_together = ("menu", "title", "parent")
+
+    def __str__(self):
+        return self.title
